feb23bb5aeb5da1ca27ea5223bec37c9c739b666
aa-ahmed-aa <ahmedkhaled36@hotmail.com>
2019-06-30 14:48:11 +0200

    seeders and database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+        Schema::create('users', function (Blueprint $table) {   
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('weekly_visits_count')->default(0);
+            $table->string('monthly_visits_count')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
